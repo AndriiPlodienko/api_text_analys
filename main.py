c2e3977296b1801f352e025c6dcdb819a0202dbb
Andrii Plodienko <109897118+AndriiPlodienko@users.noreply.github.com>
2023-04-30 19:36:56 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import openai
+import re
+
+# API Key
+openai.api_key = "AITOKEN"
+
+# Read text from a file
+with open("text.txt", "r") as f:
+    text = f.read()
+
+# Remove unnecessary characters from the text
+text = re.sub('\n+', ' ', text)  # replacing a newline character with a space
+text = re.sub('\[[0-9]*\]', '', text)  # remove numbers in square brackets
+text = re.sub('[^\w\s]', '', text)  # removing special characters
+
+# Calling the API for text analysis
+response = openai.Completion.create(
+    engine="davinci",  # модель для аналізу
+    prompt=f"Statistical analysis of the text:\n{text}",
+    max_tokens=1024,
+    n=1,
+    stop=None,
+    temperature=0.5,
+)
+
+# Displaying statistics on the screen
+result = response.choices[0].text.strip()
+print(result)
